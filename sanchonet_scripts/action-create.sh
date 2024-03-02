205201c5bef4f8c9f3d4b791dd3a59aacbbf073c
Mike Hornan [ABLE] <112007661+Hornan7@users.noreply.github.com>
2024-03-02 02:53:08 -0500

    Update action-create.sh

--- sanchonet_scripts/action-create.sh
+++ sanchonet_scripts/action-create.sh
@@ -63,8 +63,8 @@ building_gov_action() {
 time_passing_animation() {
     local animation="|/-\\"
     local dots=""
-    for _ in {1..3}; do
-        for i in $(seq 0 3); do
+    for _ in {1..5}; do
+        for i in $(seq 0 5); do
             echo -n -e "\r[ ${animation:$i:1} ] Waiting$dots"
             sleep 0.5
             dots+=".."
@@ -82,7 +82,7 @@ verify_mem_pool() {
    			echo "Your Transaction is still in the memory pool. Please Wait...."
       			time_passing_animation
 	 		MEMPOOL=$(cardano-cli conway query tx-mempool --testnet-magic 4 tx-exists $(cardano-cli conway transaction txid --tx-file action-tx.signed) | jq .exists)
-	 		sleep 2
+	 		sleep 1
     		else
       			echo "Your Transaction as been included into a block! You can now proceed to your next governance action spam"
 	 		break

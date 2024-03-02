58a3334a13e2dfb5cc0672956af775f25060dcf4
Mike Hornan [ABLE] <112007661+Hornan7@users.noreply.github.com>
2024-03-02 01:22:42 -0500

    Update action-create.sh (#17)

--- sanchonet_scripts/action-create.sh
+++ sanchonet_scripts/action-create.sh
@@ -36,7 +36,7 @@ building_gov_action() {
   		    --transfer ${AMOUNT} \
   		    --out-file action-create/action${INDEXNO}.action
                 echo " --proposal-file action-create/action${INDEXNO}.action" >> action-create/txvar.txt
-                echo -ne "\rPreparing action number ${INDEXNO} "
+                echo "Preparing action number ${INDEXNO} "
                 sleep 0.1
 		AMOUNT=$((AMOUNT-1000000))
                 INDEXNO=$((INDEXNO-1))
@@ -52,7 +52,7 @@ building_gov_action() {
                     --transfer ${AMOUNT} \
                     --out-file action-create/action${INDEXNO}.action
                 echo " --proposal-file action-create/action${INDEXNO}.action" >> action-create/txvar.txt
-                echo -ne "\rPreparing action number ${INDEXNO} "
+                echo "Preparing action number ${INDEXNO} "
 
                     sleep 0.5
 		fi      
@@ -77,7 +77,7 @@ sleep 0.2
         --out-file action-tx.raw
         
 # Remove the action index options file        
-rm -rf action-create
+rm -rf action-create 2>/dev/null
   
 echo "           Signing Transaction"
 echo "------------------------------------------"
